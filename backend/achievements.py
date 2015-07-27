7462668b68a5c6fac3f89ed91fe3717c61d93c8d
PendingChaos <pendingchaos02@gmail.com>
2015-07-27 23:02:33 +0100

    Added a new achievement.

--- backend/achievements.py
+++ backend/achievements.py
@@ -5,6 +5,7 @@ def award(condition, user, achievement):
         user["achievements"].append(achievement)
 
 def update_achievements(user, place):
+    award(user["score"] >= 1, user, "1")
     award(user["score"] >= 10, user, "10")
     award(user["score"] >= 50, user, "50")
     award(user["score"] >= 100, user, "100")
